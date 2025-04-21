79d8ef20f02586a77bd4799c2ef4296cb6acac67
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-21 16:01:11 -0500

    perf: :zap: Fue tedioso pero se logro la funcion de iniciar el juego.

--- main.js
+++ main.js
@@ -11,3 +11,29 @@ const ctx = canvas.getContext("2d");
 const contenedorPalabra = document.getElementById("word-container");
 const tecladoContainer = document.getElementById("teclado-container");
 const botonReiniciar = document.getElementById("reiniciar-btn");
+
+function iniciarJuego() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    contenedorPalabra.innerHTML = "";
+    tecladoContainer.innerHTML = "";
+    errores = 0;
+
+    palabraOculta = palabras[Math.floor(Math.random() * palabras.length)];
+    console.log("Palabra seleccionada:", palabraOculta);
+
+    for (let i = 0; i < palabraOculta.length; i++) {
+        const span = document.createElement("span");
+        span.textContent = "_";
+        span.setAttribute("data-index", i);
+        contenedorPalabra.appendChild(span);
+    }
+
+    
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    letras.split("").forEach(letra => {
+        const boton = document.createElement("button");
+        boton.textContent = letra;
+        boton.addEventListener("click", () => verificarLetra(letra.toLowerCase(), boton));
+        tecladoContainer.appendChild(boton);
+    });
+}
