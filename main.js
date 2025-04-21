5425ae4dae313a8a3e827bd889a0adc0234e9aa6
kevincito0987 <kevinangaritacaamano@gmail.com>
2025-04-21 15:59:07 -0500

    perf: :zap: Listo las constantes de palabras errores y el maximo de errores permitidos.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log("Este es el código de la página principal, que se ejecutará cuando se cargue la página.");
+
+const palabras = ["javascript", "frontend", "developer", "programacion", "juego", "ahorcado", "html", "css"];
+let palabraOculta = "";
+let errores = 0;
+const maxErrores = 11;
